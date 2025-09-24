b26cd897034243c3ee7d1f8853f32039f778326f
12fauzan <46532457+12fauzan@users.noreply.github.com>
2025-09-24 08:06:10 +0800

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,8 @@
-
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Portal Link 🚀</h1>
+      <p>Selamat datang di portal dummy. Nanti kita tambah login, kategori, dan grafik.</p>
+    </div>
+  )
+}
